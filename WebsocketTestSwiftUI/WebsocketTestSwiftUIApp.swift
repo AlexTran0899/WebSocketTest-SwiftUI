be3f5d1a0435ae744d664f3297a7ebee35b08681
AlexTran0899 <alextran0899@gmail.com>
2023-03-29 11:55:36 -0500

    Initial Commit

---
+++ WebsocketTestSwiftUI/WebsocketTestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebsocketTestSwiftUIApp.swift
+//  WebsocketTestSwiftUI
+//
+//  Created by Alex Tran on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WebsocketTestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
